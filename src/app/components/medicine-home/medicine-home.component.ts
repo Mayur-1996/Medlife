a6ffa4c18ce071afe76358265ce8712c7b6342ae
Mayur-1996 <chaudharimayur708@gmail.com>
2023-04-03 13:17:17 +0530

    top-deals implemented

---
+++ src/app/components/medicine-home/medicine-home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/core/services/http.service';
+
+@Component({
+  selector: 'app-medicine-home',
+  templateUrl: './medicine-home.component.html',
+  styleUrls: ['./medicine-home.component.scss']
+})
+export class MedicineHomeComponent implements OnInit {
+
+  Pincode:string ="";
+  pincodeDetails:any;
+  displayErrorMessage:boolean = false;
+  isPinCodeAvailable:boolean =false;
+  showDefaultPinCode : boolean= true;
+ 
+constructor(private http:HttpService) { }
+
+  ngOnInit(): void {
+   
+  }
+ 
+  VerifyPincode() {
+    const endPoint = "pin-code-details?"+"pincode" + this.Pincode
+    this.http.getDataFromServer(endPoint).subscribe((el:any)=>{
+      if(el && el.length > 0){
+        this.pincodeDetails = el[0];
+        this.displayErrorMessage = false;
+        this.showDefaultPinCode = false;
+      }else {
+       this.displayErrorMessage = true;
+       this.showDefaultPinCode = true;
+
+      }
+    }
+    )
+  }
+}
+
---
+++ src/app/core/services/http.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+ baseUrl:string=environment.baseUrl;
+ httpheaders:HttpHeaders = new HttpHeaders()
+                           .set("Content-type","application/json");
+
+  constructor(private http:HttpClient) { }
+
+  getDataFromServer(endPoint:string){ 
+  const url = this.baseUrl + endPoint;
+  return this.http.get(url,{headers:this.httpheaders})
+  }
+  
+}
